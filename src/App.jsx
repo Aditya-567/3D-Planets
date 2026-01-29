c0858f2bbd98f29c0227f0e9a22bd1b780e5971d
Aditya-567 <lextrone000567@gmail.com>
2026-01-29 15:04:17 +0530

    earth and satelite

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 import EarthandMoon from "./components/EarthandMoon";
+import EarthMoonSatalite from "./components/EarthMoonSatalite";
 
 
 const App = () => {
   return (
     <BrowserRouter>
       <EarthandMoon />
+      <EarthMoonSatalite />
     </BrowserRouter>
   );
 }
